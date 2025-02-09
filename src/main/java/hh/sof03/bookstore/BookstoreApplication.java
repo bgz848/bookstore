ba34e2d0b789050f0523d5d6dfc813f5dd84d162
bgz848 <bgz848@myy.haaga-helia.fi>
2025-02-09 20:54:03 +0200

    Proper initial commit

---
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -0,0 +1,26 @@
+package hh.sof03.bookstore;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof03.bookstore.domain.Book;
+
+import hh.sof03.bookstore.domain.BookRepository;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+    public CommandLineRunner dataLoader(BookRepository bookRepository) {
+        return args -> {
+            bookRepository.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21", 45.0));
+            bookRepository.save(new Book("Animal Farm", "George Orwell", 1945, "2212343-5", 50.0));
+        };
+    }
+}
---
+++ src/main/java/hh/sof03/bookstore/domain/BookRepository.java
@@ -0,0 +1,6 @@
+package hh.sof03.bookstore.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
