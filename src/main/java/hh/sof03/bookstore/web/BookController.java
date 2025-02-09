275b3de81bb1aca3f48508fb5f88e8e00a90ff15
bgz848 <bgz848@myy.haaga-helia.fi>
2025-02-09 21:42:38 +0200

    CRUD

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -6,6 +6,9 @@ import hh.sof03.bookstore.domain.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -21,5 +24,23 @@ public class BookController {
         List<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
         return "booklist"; // This maps to booklist.html
+    }
+
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook"; 
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
     }
 }
