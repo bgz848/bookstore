6399e3bab6ef12c00de2628ba01ea3bf2b1858cf
bgz848 <bgz848@myy.haaga-helia.fi>
2025-02-23 20:10:48 +0200

    Added categories

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -1,13 +1,16 @@
 package hh.sof03.bookstore;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import hh.sof03.bookstore.domain.Book;
+import hh.sof03.bookstore.domain.Category;
 
 import hh.sof03.bookstore.domain.BookRepository;
+import hh.sof03.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -16,9 +19,14 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-	@Bean
-    public CommandLineRunner dataLoader(BookRepository bookRepository) {
+    @Bean
+    public CommandLineRunner dataLoader(BookRepository bookRepository, CategoryRepository categoryRepository) {
         return args -> {
+            Category sciFi = new Category("Romance");
+            Category fantasy = new Category("Fantasy");
+            categoryRepository.save(sciFi);
+            categoryRepository.save(fantasy);
+    
             bookRepository.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21"));
             bookRepository.save(new Book("Animal Farm", "George Orwell", 1945, "2212343-5"));
         };
